bf9e28c462cc19e0b399618cd6b76785f11ec3a0
Mayank <hellomayankashok@gmail.com>
2023-03-04 14:41:40 -0800

    create a student profile page and mock data.  render same in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import StudentProfileCard from './components/StudentProfile'
+import { userProfileProps } from './components/mocks/studentProfileMock'
 
-function App() {
+const { name, profilePictureUrl, email, phoneNumber, major, gpa } = userProfileProps
+
+const App = (): JSX.Element => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <StudentProfileCard
+    name={name} profilePictureUrl={profilePictureUrl} email={email} phoneNumber={phoneNumber} major={major} gpa={gpa}
+    />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/mocks/studentProfileMock.ts
@@ -0,0 +1,23 @@
+export const userProfileProps = {
+  name: 'John Doe',
+  profilePictureUrl: './assets/profile_pic.jpeg',
+  email: 'johndoe@example.com',
+  phoneNumber: '123-456-7890',
+  major: 'Computer Science',
+  gpa: 3.8,
+  workExperience: ['Software Developer at Acme Inc.', 'Web Developer at XYZ Corp.'],
+  skills: ['React', 'JavaScript', 'CSS', 'HTML'],
+  education: [
+    { school: 'University of California, Los Angeles', degree: 'Bachelor of Science in Computer Science', coursework: 'Data Structures, Algorithms, Databases, Software Engineering' },
+    { school: 'California State University, Northridge', degree: 'Master of Science in Computer Science', coursework: 'Machine Learning, Natural Language Processing, Distributed Systems' }
+  ],
+  references: [
+    { name: 'Jane Smith', position: 'Professor of Computer Science', email: 'janesmith@example.com', phone: '123-456-7890' },
+    { name: 'Bob Johnson', position: 'Manager at Acme Inc.', email: 'bobjohnson@example.com', phone: '123-456-7890' }
+  ],
+  testimonials: [
+    { name: 'Sarah Lee', quote: 'John is an excellent programmer and a pleasure to work with.' },
+    { name: 'David Kim', quote: 'John is a great team player and always goes the extra mile to ensure project success.' }
+  ],
+  personalStatement: 'I am a passionate and hard-working software developer with experience in front-end and back-end development. My goal is to use my skills to create innovative solutions that make a positive impact on society.'
+}
