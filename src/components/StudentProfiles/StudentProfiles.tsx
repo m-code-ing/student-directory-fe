6c74d62211790141226c629da346dd617a2b3a95
Mayank <hellomayankashok@gmail.com>
2023-03-05 10:19:26 -0800

    add background color to students page

--- src/components/StudentProfiles/StudentProfiles.tsx
+++ src/components/StudentProfiles/StudentProfiles.tsx
@@ -1,26 +1,29 @@
-import { Grid } from '@mui/material'
+import { Box, Grid, useTheme } from '@mui/material'
 import { userProfileProps } from '../mocks/studentProfileMock'
 import StudentProfileCard from './StudentProfileCard'
 
 const { name, profilePictureUrl, email, phoneNumber, major, gpa } = userProfileProps
 
 const StudentProfiles = (): JSX.Element => {
+  const theme = useTheme()
   const studens = [1, 2, 3, 4, 5]
   return (
-    <Grid mt={1} container spacing={2} justifyContent="center">
-      {studens.map((key, index) => (
-        <Grid item xs={10} md={4} m={1} key={index}>
-          <StudentProfileCard
-            name={name}
-            profilePictureUrl={profilePictureUrl}
-            email={email}
-            phoneNumber={phoneNumber}
-            major={major}
-            gpa={gpa}
-          />
-        </Grid>
-      ))}
-    </Grid>
+    <Box bgcolor={theme.palette.custom.bg.dark}>
+      <Grid mt={1} container spacing={2} justifyContent="center">
+        {studens.map((key, index) => (
+          <Grid item xs={10} md={4} m={1} key={index}>
+            <StudentProfileCard
+              name={name}
+              profilePictureUrl={profilePictureUrl}
+              email={email}
+              phoneNumber={phoneNumber}
+              major={major}
+              gpa={gpa}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   )
 }
 
